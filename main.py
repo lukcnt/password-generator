dd1907ec60ee15e20d65ff00c634d92ccd93305a
Lucas Cantoni <lukcnt@outlook.com>
2022-08-15 23:10:33 -0300

    Add the letters, number and symbols that will be used to create the password using the string module

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+import string
+
 print("Welcome to the LukCnt Password Generator!")
 
 quantity_of_letters = int(input("How many letters would you like in your password?\n"))
 quantity_of_numbers = int(input("How many numbers would you like?\n"))
 quantity_of_symbols = int(input("How many symbols would you like?\n"))
+
+letters = list(string.ascii_letters)
+numbers = list(string.digits)
+symbols = list(string.punctuation)
