11ff8e8f08e43b348d02b7ddfb5b2666177c918a
Lucas Cantoni <lukcnt@outlook.com>
2022-08-15 22:49:56 -0300

    Creates the input of how many letter, numbers and symbols the user wants in the password

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
 print("Welcome to the LukCnt Password Generator!")
+
+quantity_of_letters = int(input("How many letters would you like in your password?\n"))
+quantity_of_numbers = int(input("How many numbers would you like?\n"))
+quantity_of_symbols = int(input("How many symbols would you like?\n"))
